eec4b9ee89267f64a473a2084b4f33bd407c3507
catarak <ctarakajian@gmail.com>
2014-11-24 16:41:46 -0500

    fixed nested form error

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -16,6 +16,7 @@ class Users::InvitationsController < Devise::InvitationsController
       Membership.create(user_id: self.resource.id, group_id: params[:group_id], role: :regular)
       @group = Group.find(params[:group_id])
       @user = self.resource
+
     else
       respond_with_navigational(resource) { render :new }
     end

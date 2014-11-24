6781ea059d446c563f4f79f84d898224d57ee40b
sammarcus <sam.marcus@me.com>
2014-11-24 16:08:24 -0500

    fixed form, back to working. still need to add styling

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ gem 'autoprefixer-rails'
 
 gem "nested_form"
 
+
 group :development, :test do
   # Call 'debugger' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
---
+++ app/assets/javascripts/accounts.js
@@ -0,0 +1,5 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");

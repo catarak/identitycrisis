fc281f7fb54cf07db8cdd303ce145d5c75c407a9
catarak <ctarakajian@gmail.com>
2014-11-24 00:21:05 -0500

    add ajax request to add new member to list of members

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,13 +1,23 @@
 class Users::InvitationsController < Devise::InvitationsController
+  def new
+    @group = Group.find(params[:group_id])
+    self.resource = resource_class.new
+    render :new
+  end
+
   def create
     self.resource = invite_resource
     resource_invited = resource.errors.empty?
 
     yield resource if block_given?
 
-    if !resource_invited
+    #this is broken since we are making an ajax call
+    if resource_invited
+      Membership.create(user_id: self.resource.id, group_id: params[:group_id], role: :regular)
+      @group = Group.find(params[:group_id])
+      @user = self.resource
+    else
       respond_with_navigational(resource) { render :new }
     end
   end
-
 end

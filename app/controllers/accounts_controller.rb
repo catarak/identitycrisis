72c2435c8504768fd223ca4c59e9db242e58cea6
catarak <ctarakajian@gmail.com>
2014-11-23 19:56:13 -0500

    add account from group show page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,17 +5,21 @@ class AccountsController < ApplicationController
 
   def new
     @account = Account.new
+    @group = Group.find(params[:group_id])
+    #run js to render new account form
   end
 
   def create
     @account = Account.new(account_params)
     if @account.save
-     
+      render 'create'
+    else
+      render json: {error: "Your account failed to save"}, status: 400
     end
   end
 
   private
     def account_params
-      params.require(:account).permit(:name, :access_token, :access_token_secret)
+      params.require(:account).permit(:name, :access_token, :access_token_secret, :group_id)
     end
 end

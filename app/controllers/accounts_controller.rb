8ab782450ff12ad95911061a16c4db3e3814dfcb
Julia <juliazhang86@gmail.com>
2014-11-25 18:22:07 -0500

    Added account before filter

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,7 @@
 class AccountsController < ApplicationController
 
   before_filter :current_member?, :except => [:index]
+  
   def show
     @account = Account.find(params[:id])
     @recent_tweets = TwitterApi.new(@account.access_token, @account.access_token_secret).grab_10_most_recent(@account.name)
